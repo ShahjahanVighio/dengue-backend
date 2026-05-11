c1adf7487428dcb8c5243181ef0f5b2b06f855b9
Shahjahan <shahjahanvighio1@gmail.com>
2026-05-12 03:24:54 +0500

    Update api.py

--- api.py
+++ api.py
@@ -33,5 +33,5 @@ def predict():
 
 if __name__ == '__main__':
     # Railway ke liye port aur host set karna zaroori hai
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
